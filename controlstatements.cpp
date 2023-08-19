b84aa53608faa151e16162a48055ed9f6b74ac75
Cecil <cecioasuna@gmail.com>
2023-08-19 15:45:53 +0300

    while do while

--- controlstatements.cpp
+++ controlstatements.cpp
@@ -3,31 +3,33 @@ using namespace std;
 
 int main()
 {
-    int arr[] = {125, 3525,34,80,78,87};
-    int newarr;
-    
+    //int arr[] = {125, 3525,34,80,78,87};
+    int num, sum, product;
+    sum = 0;
     /*
-    cout<<"Enter a number and your name: ";
-    cin>>newarr>>name;
-    cout<<"Number is: "<<newarr<<endl;
-    cout<<"Name is: "<<name<<endl;
-    */
-   cout<<"Enter marks: ";
-   cin>>newarr;
+    cout<<"Enter a number: ";
+    cin>>num;
 
-   if(newarr>=0){
-    if(newarr<=65){
-        cout<<"Grade : D";
-    }else if(newarr>65 && newarr<=75){
-        cout<<"Grade : C";
-    }else if(newarr>75 && newarr<=90){
-        cout<<"Grade : B";
-    }else if(newarr>90 && newarr<=100){
-        cout<<"Grade : A";
-    }else{
-        cout<<"ENTER VALID MARKS";
+    for(int i=1; i<=num; i++)
+    {
+        cout<<i<<" ";        
+        sum += i;
+        
     }
+    cout<<"Sum of  from 1 to "<<num<<" is "<<sum;
+    */
+   //Find the factorial of a given number
+   product = 1;
+   cout<<"Enter a number: ";
+   cin>>num;
+
+   for(int i = 2; i <= num; i++) //you can initialize to 1 or 2
+   {
+    cout<<i<<" ";
+    product *= i;
    }
+   cout<<"Factorial of "<<num<<" is "<<product;
+   
 
     return 0;
 }
