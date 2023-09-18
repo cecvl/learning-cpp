7d743dc226dab1e1e97d7753b9df6b77a94e05a1
Christian Cecil <cecioasuna@gmail.com>
2023-09-18 13:30:17 +0300

    Update controlstatements.cpp

--- loops/controlstatements.cpp
+++ loops/controlstatements.cpp
@@ -6,29 +6,27 @@ int main()
     //int arr[] = {125, 3525,34,80,78,87};
     int num, sum, product;
     sum = 0;
-    /*
+    
     cout<<"Enter a number: ";
     cin>>num;
 
     for(int i=1; i<=num; i++)
-    {
-        cout<<i<<" ";        
+    {   cout<<i<<" ";        
         sum += i;
-        
-    }
+     }
     cout<<"Sum of  from 1 to "<<num<<" is "<<sum;
-    */
+    
    //Find the factorial of a given number
    product = 1;
-   cout<<"Enter a number: ";
-   cin>>num;
+   cout << "Enter a number: ";
+   cin >> num;
 
    for(int i = 2; i <= num; i++) //you can initialize to 1 or 2
    {
-    cout<<i<<" ";
+    cout << I << " ";
     product *= i;
    }
-   cout<<"Factorial of "<<num<<" is "<<product;
+   cout << "Factorial of " << num << " is " << product;
    
 
     return 0;
