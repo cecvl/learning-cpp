5f691b1dc6613c3be210789879beaf1bbd0607a6
Cecil <cecioasuna@gmail.com>
2023-09-30 17:07:01 +0300

    update

--- class objects/constructor.cpp
+++ class objects/constructor.cpp
@@ -1,23 +1,45 @@
 #include <iostream>
 using namespace std;
 //constructor same name as class 
+//used to initialize objects
+//default, copy, parametized
 
-class Wall{
-    private: 
-        double length;// data member
-        double height;
+// declare a class
+class Wall {
+  private:
+    double length;
+    double height;
 
-    public:        
-        Wall(double len, double hei){
-            length = len;
-            height = hei;
-        }
-        double calculateArea(){
-            return length * height;
-            }
+  public:
+
+    // initialize variables with parameterized constructor
+    Wall(double len, double hgt) {
+      length = len;
+      height = hgt;
+    }
+
+    // copy constructor with a Wall object as parameter
+    // copies data of the obj parameter
+    Wall(Wall &obj) {
+      length = obj.length;
+      height = obj.height;
+    }
+
+    double calculateArea() {
+      return length * height;
+    }
 };
-int main()
-{
-    Wall wall1(23.3,34.2); //constructor can use private data members
-    cout << "Area = " << wall1.calculateArea();
+
+int main() {
+  // create an object of Wall class
+  Wall wall1(10.5, 8.6);
+
+  // copy contents of wall1 to wall2
+  Wall wall2 = wall1;
+
+  // print areas of wall1 and wall2
+  cout << "Area of Wall 1: " << wall1.calculateArea() << endl;
+  cout << "Area of Wall 2: " << wall2.calculateArea();
+
+  return 0;
 }
