0e5dd4640fda3aae40e883822b8e0a3525cee332
Cecil <cecioasuna@gmail.com>
2023-10-15 22:05:15 +0300

    file input

--- advanced_2_0/file handling/createfile.cpp
+++ advanced_2_0/file handling/createfile.cpp
@@ -4,13 +4,13 @@
 using namespace std;
 int main()
 {
-    fstream trialone; //object of fstream class
-    trialone.open("trialone.txt", ios::out);
+    fstream trialone("trialone.txt", ios::app); //ios::out to create a new file
     if (!trialone){
         cout << "Error opening file" << endl;
         
     }else{
         cout << "File opened successfully" << endl;
+        trialone << "Hello World" << endl;
         trialone.close();
     }
     //trialone.close();
